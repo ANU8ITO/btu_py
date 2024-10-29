92bf16aee794585065661fb925768437c76206c1
ANU8ITO <giorgi.tefnadze.1@btu.edu.ge>
2024-10-29 13:06:57 +0400

    Update netflix.py

--- netflix.py
+++ netflix.py
@@ -16,6 +16,14 @@ class NetflixAccount:
     def get_account_limit(self):
         return f"{self.account_name} Your account limit is {self.__account_limit}"
 
+    def watch_video(self):
+        if(self.account_views < self.__account_limit):
+            self.account_views += 1
+            print(f"{self.account_name} has watched a video. Remaining views: {self.__account_limit - self.account_views}")
+    
+    def add_view_limit(self):
+        self.__account_limit += 100
+        print(f"{self.account_name} your view limit increased. new view limit - {self.__account_limit}")
     
 class BasicAccount(NetflixAccount):
     def __init__(self, account_name):
@@ -29,10 +37,22 @@ class VIPAccount(NetflixAccount):
     def __init__(self, account_name):
         super().__init__(account_name, account_type="vip")
         
+class NetflixCard:
+    def __init__(self, card_holder, card_type, netflix_account):
+       self.card_holder = card_holder
+       self.card_type = card_type
+       self.netflix_account = netflix_account
+       
+    def get_card_info(self):
+        return f"Card Holder: {self.card_holder}, Card Type: {self.card_type}"
         
         
 basic = BasicAccount("george")
 print(basic.get_account_limit())
-            
+basic.watch_video()
+basic.add_view_limit()
+
+basic_card = NetflixCard("george", "digital", basic)
+print(basic_card.get_card_info())
     
         
